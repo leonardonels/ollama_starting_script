8c3d928f8faccdb02bbc58694a68ef8571ce48b0
Leonardo Nels <81677769+leonardonels@users.noreply.github.com>
2024-06-15 08:53:49 +0200

    Update crafty_server.sh

--- crafty_server.sh
+++ crafty_server.sh
@@ -1,7 +1,25 @@
 #!/bin/sh
 
+# script crafty_server.sh
+
 echo -e "\nWARNING - remember to put a . (dot) befor the script: . ./ollama_server.sh\n"
 
-sudo su crafty
-cd /var/opt/minecraft/crafty
-./run_crafty.sh
+# Controlla l'utente che ha avviato il file
+
+if [ "$(whoami)" != "root" ]; then
+
+    # Se non è root, cambia directory e esegue la startup di Crafty
+
+    echo "Sei un utente regolare. Cambio directory e iniziamo l'esecuzione di Crafty."
+
+    cd /var/opt/minecraft/crafty/run_crafty.sh
+
+else
+
+    # Se è root, utilizza sudo per avviare Crafty
+
+    echo "E' utente root. Ho eseguito 'sudo crafty' come richiesto."
+
+    sudo crafty &
+
+fi
